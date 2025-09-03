623ae43b4a9d8202425211523900ae9eb7f7b6f2
ahsanparvezz <ahsan.p1012@gmail.com>
2025-09-03 17:40:17 +0700

    cleanup

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'nama_aplikasi' : 'Football Shop',
+        'app_name' : 'Football Shop',
         'name': 'Ahsan Parvez',
         'class': 'PBP E'
     }
